0fc3c4fe9661eba4532174f1de53d1b675a4f410
Andy Clemenko <clemenko@gmail.com>
2016-05-17 15:23:59 +0000

    adding auto-licnesing

--- ucp.sh
+++ ucp.sh
@@ -2,19 +2,14 @@
 ###################################
 # edit vars
 ###################################
-num=4
+num=3
 prefix=ucp
 project=dev
 image="coreos_1010"
-#image="CentOS 7.latest"
-USER=core
 flavor=mash.memory.small
 key_name=labnc_186
-#gui admin password
 password=Pa22word
-version=v1.0.1
-
-rancher=false
+license_file="/home/clemenko/Downloads/docker_subscription.lic"
 
 ################################# up ################################
 function build () {
@@ -38,55 +33,39 @@ function up () {
 
 sleep 30
 
- echo -n " checking for ssh."
- for ext in $(cat hosts.txt|awk '{print $3}'); do
-   until [ $(ssh -o ConnectTimeout=1 $USER@$ext 'exit' 2>&1 | grep 'timed out' | wc -l) = 0 ]; do echo -n "." ; done
- done
- echo ""
+echo -n " checking for ssh."
+for ext in $(cat hosts.txt|awk '{print $3}'); do
+ until [ $(ssh -o ConnectTimeout=1 core@$ext 'exit' 2>&1 | grep 'timed out' | wc -l) = 0 ]; do echo -n "." ; done
+done
+echo ""
 
- host_list=$(cat hosts.txt|awk '{printf $3","}'|sed 's/,$//')
+host_list=$(cat hosts.txt|awk '{printf $3","}'|sed 's/,$//')
 
  #add etc hosts
- etc_hosts_cmd=$(cat hosts.txt|awk '{printf "echo "$2" "$1"| sudo tee --append /etc/hosts;"}'|sed 's/.$//')
- pdsh -l $USER -w $host_list 'chmod u+w /etc/hosts; sed -i -e "/127.0.0.1 $HOSTNAME/d" -e "/::1 $HOSTNAME/d" /etc/hosts'
- #pdsh -l core -w $host_list "$etc_hosts_cmd" > /dev/null 2>&1
+ #etc_hosts_cmd=$(cat hosts.txt|awk '{printf "echo "$2" "$1"| sudo tee --append /etc/hosts;"}'|sed 's/.$//')
+ #pdsh -l core -w $host_list 'chmod u+w /etc/hosts; sed -i -e "/127.0.0.1 $HOSTNAME/d" -e "/::1 $HOSTNAME/d" /etc/hosts'
 
+echo " starting ucp server."
 
- echo -n " starting ucp server."
- server=$(cat hosts.txt|head -1|awk '{print $3}')
+server=$(cat hosts.txt|head -1|awk '{print $3}')
+fingerprint=$(ssh core@$server "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --admin-password $password --host-address $server" 2>&1 |grep Fingerprint|awk '{print $7}'|sed -e 's/Fingerprint=//g' -e 's/"//g')
 
+echo $fingerprint > fingerprint.txt
 
- echo " setting up nodes."
+echo " adding licenses."
+token=$(curl -sk "https://$server/auth/login" -X POST -d '{"username":"admin","password":"Pa22word"}'|jq -r .auth_token)
+curl -k "https://$server/api/config/license" -X POST -H "Authorization: Bearer $token" -d "{\"auto_refresh\":true,\"license_config\":$(cat $license_file |jq .)}"
 
-exit
- ssh $USER@$server "docker run -d -p 8080:8080 --restart=always --name rancher-server rancher/server:$version" > /dev/null 2>&1
- until curl $server:8080 > /dev/null 2>&1; do echo -n .; sleep 1; done
- echo " "
+echo " setting up nodes."
+node_list=$(cat hosts.txt |grep -v "$server"|awk '{printf $3","}')
+pdsh -l core -w $node_list "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp join --admin-username admin --admin-password $password --fingerprint $fingerprint --url https://$server" > /dev/null 2>&1
 
- echo " setting up rancher server"
- curl -s 'http://'$server':8080/v1/activesettings/1as!api.host?projectId=user' -X PUT -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' --data '{"id":"1as!api.host","type":"activeSetting","name":"api.host","activeValue":null,"inDb":false,"source":null,"value":"'$server':8080"}' > /dev/null 2>&1
- curl -s 'http://'$server':8080/v1/registrationtokens?projectId=1a5' -X POST -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' -H 'Referer: http://'$server':8080/static/hosts/add/custom' > /dev/null 2>&1
- sleep 10
-
- echo " - attaching agents"
- agent_string=$(curl -s http://$server:8080/v1/registrationtokens?projectId=1a5|sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w command|sed 's/command|sudo//g')
- echo $agent_string > agent_string.txt
- agent_list=$(cat hosts.txt|tail -n +2|awk '{printf $3","}')
- pdsh -l $USER -w $agent_list "$agent_string" > install.log 2>&1
-
- echo " - setting up api keys"
- api_string=$(curl -s 'http://'$server':8080/v1/apikey?projectId=1a5' -X POST -H 'Accept: application/json' --data {"type":"apikey"})
- RANCHER_ACCESS_KEY=$(echo $api_string|sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g'|grep publicValue|awk -F"|" '{print $2}')
- RANCHER_SECRET_KEY=$(echo $api_string|sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g'|grep secretValue|awk -F"|" '{print $2}')
- echo $RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY > secrets.txt
-
- echo " - setting up security"
- curl -s 'http://'$server':8080/v1/localauthconfig' -X POST -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' --data '{"accessMode":"unrestricted","name":"admin","id":null,"type":"localAuthConfig","enabled":false,"password":"'$password'","username":"admin"}' > /dev/null 2>&1
- curl -s 'http://'$server':8080/v1/token' -X POST -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' --data '{"code":"admin:'$password'","authProvider":"localauthconfig"}' > /dev/null 2>&1
- curl -s 'http://'$server':8080/v1/localauthconfig' -X POST -H 'Accept: application/json' -H 'Content-Type: application/json; charset=UTF-8' --data '{"accessMode":"unrestricted","name":"admin","id":null,"type":"localAuthConfig","enabled":true,"password":"'$password'","username":"admin"}' > /dev/null 2>&1
+echo " restarting docker daemons"
+ pdsh -l core -w $host_list "sudo systemctl restart docker"
 
 echo ""
 echo "========= UCP install complete ========="
+echo " please wait a minute for the docker daemons to restart."
 status
 }
 
@@ -100,22 +79,14 @@ rm -rf *.txt *.log
 
 ############################# status ################################
 function status () {
-  server=$(cat hosts.txt|head -1|awk '{print $3}')
-  RANCHER_ACCESS_KEY=$(cat secrets.txt|awk -F":" '{print $1}')
-  RANCHER_SECRET_KEY=$(cat secrets.txt|awk -F":" '{print $2}')
   echo "===== Cluster ====="
   supernova $project list |grep $prefix
   echo ""
   echo "===== Dashboards ====="
-  echo "Rancher : http://"$server":8080"
+  echo " - server   : https://$server"
   echo " - username : admin"
   echo " - password : "$password
   echo ""
-  echo "===== Secret Keys ====="
-  echo "export RANCHER_URL=http://"$server":8080/v1/projects/1a5"
-  echo "export RANCHER_ACCESS_KEY="$RANCHER_ACCESS_KEY
-  echo "export RANCHER_SECRET_KEY="$RANCHER_SECRET_KEY
-  echo "export NIFI_MASTER=$(cat hosts.txt |sed -n '2p'|awk '{print $2}')"
 }
 
 case "$1" in
