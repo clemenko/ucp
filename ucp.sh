ad2383eae51ef6283f5718770a12f984c9d8b726
Andy Clemenko <clemenko@gmail.com>
2016-07-13 14:01:51 -0700

    updated for curl|bash for docker engine

--- ucp.sh
+++ ucp.sh
@@ -8,9 +8,10 @@ password=Pa22word
 zone=nyc2
 size=2gb
 key=30:98:4f:c5:47:c2:88:28:fe:3c:23:cd:52:49:51:01
-image=coreos-stable
+#image=coreos-stable
+image=ubuntu-16-04-x64
 password=Pa22word
-license_file="../docker_subscription.lic"
+license_file="docker_subscription.lic"
 
 export PDSH_RCMD_TYPE=ssh
 ################################# up ################################
@@ -33,16 +34,20 @@ doctl compute droplet list|grep -v ID|grep $prefix|awk '{print $3" "$2}'> hosts.
 
 echo -n " checking for ssh."
 for ext in $(cat hosts.txt|awk '{print $1}'); do
-  until [ $(ssh -o ConnectTimeout=1 core@$ext 'exit' 2>&1 | grep 'timed out' | wc -l) = 0 ]; do echo -n "." ; done
+  until [ $(ssh -o ConnectTimeout=1 root@$ext 'exit' 2>&1 | grep 'timed out' | wc -l) = 0 ]; do echo -n "." ; done
 done
+sleep 15
 echo ""
 
 host_list=$(cat hosts.txt|awk '{printf $1","}'|sed 's/,$//')
+echo " installing latest docker"
+pdsh -l root -w $host_list 'curl -fsSL https://get.docker.com/ | sh; systemctl enable docker; systemctl start docker' > /dev/null 2>&1
+
 
 echo " starting ucp server."
 
 server=$(cat hosts.txt|head -1|awk '{print $1}')
-fingerprint=$(ssh core@$server "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --admin-password $password --host-address $server" 2>&1 |grep Fingerprint|awk '{print $7}'|sed -e 's/Fingerprint=//g' -e 's/"//g')
+fingerprint=$(ssh root@$server "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --admin-password $password --host-address $server" 2>&1 |grep Fingerprint|awk '{print $7}'|sed -e 's/Fingerprint=//g' -e 's/"//g')
 
 sleep 5
 
@@ -52,14 +57,14 @@ curl -k "https://$server/api/config/license" -X POST -H "Authorization: Bearer $
 
 echo " setting up nodes."
 node_list=$(cat hosts.txt |grep -v "$server"|awk '{printf $1","}')
-pdsh -l core -w $node_list "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp join --admin-username admin --admin-password $password --fingerprint $fingerprint --url https://$server" > /dev/null 2>&1
+pdsh -l root -w $node_list "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp join --admin-username admin --admin-password $password --fingerprint $fingerprint --url https://$server" > /dev/null 2>&1
 
 echo " downloading certs"
 AUTHTOKEN=$(curl -sk -d '{"username":"admin","password":"$password"}' https://$server/auth/login | jq -r .auth_token)
 curl -sk -H "Authorization: Bearer $AUTHTOKEN" https://$server/api/clientbundle -o bundle.zip
 
 echo " restarting docker daemons"
- pdsh -l core -w $host_list "sudo systemctl restart docker"
+ pdsh -l root -w $host_list "sudo systemctl restart docker"
 
 echo ""
 echo "========= UCP install complete ========="
