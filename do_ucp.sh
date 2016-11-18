40880cea1e00d0743ba8a76cf921fe5382b3681f
Andy Clemenko <clemenko@gmail.com>
2016-11-18 16:33:21 -0500

    updated for ucp

--- do_ucp.sh
+++ do_ucp.sh
@@ -2,8 +2,8 @@
 ###################################
 # edit vars
 ###################################
-num=6  #4 or larger please!
-prefix=ucp
+num=4  #3 or larger please!
+prefix=ddc
 password=Pa22word
 zone=nyc1
 size=1gb
@@ -15,28 +15,20 @@ license_file="docker_subscription.lic"
 
 ######  NO MOAR EDITS #######
 ################################# up ################################
-function build () {
- uuid=$(uuidgen| awk -F"-" '{print $2}')
- echo " building : $prefix-$uuid "
- doctl compute droplet create $prefix-$uuid --region $zone --image $image --size $size --ssh-keys $key  > /dev/null 2>&1
-}
 
 function up () {
 export PDSH_RCMD_TYPE=ssh
-secret=$(uuidgen|sed 's/-//g')
-echo $secret > secret.txt
-
- if [ -f hosts.txt ]; then echo "hosts.txt found. Are you sure you want to override it? "; exit; fi
-
- #build
- for i in $(seq 1 $num); do
-   build
- done
-
-sleep 30
+build_list=""
+uuid=""
+for i in $(seq 1 $num); do
+ uuid=$(uuidgen| awk -F"-" '{print $2}')
+ build_list="$prefix-$uuid $build_list"
+done
+echo " building vms - $build_list"
+doctl compute droplet create $build_list --region $zone --image $image --size $size --ssh-keys $key --wait > /dev/null 2>&1
 doctl compute droplet list|grep -v ID|grep $prefix|awk '{print $3" "$2}'> hosts.txt
 
-echo -n " checking for ssh."
+echo -n " checking for ssh"
 for ext in $(cat hosts.txt|awk '{print $1}'); do
   until [ $(ssh -o ConnectTimeout=1 root@$ext 'exit' 2>&1 | grep 'timed out' | wc -l) = 0 ]; do echo -n "." ; sleep 5; done
 done
@@ -46,13 +38,11 @@ echo ""
 host_list=$(cat hosts.txt|awk '{printf $1","}'|sed 's/,$//')
 
 #setting nodes
-manager1=$(cat hosts.txt|sed -n 1p|awk '{print $1}')
-manager2=$(cat hosts.txt|sed -n 2p|awk '{print $1}')
-manager3=$(cat hosts.txt|sed -n 3p|awk '{print $1}')
-dtr_server=$(cat hosts.txt|sed -n 4p|awk '{printf $1}')
-dtr_node=$(cat hosts.txt|sed -n 4p|awk '{printf $2}')
-dtr2_server=$(cat hosts.txt|sed -n 6p|awk '{printf $1}')
-dtr2_node=$(cat hosts.txt|sed -n 6p|awk '{printf $2}')
+controller1=$(cat hosts.txt|sed -n 1p|awk '{print $1}')
+#manager2=$(cat hosts.txt|sed -n 2p|awk '{print $1}')
+#manager3=$(cat hosts.txt|sed -n 3p|awk '{print $1}')
+dtr_server=$(cat hosts.txt|sed -n 3p|awk '{printf $1}')
+dtr_node=$(cat hosts.txt|sed -n 3p|awk '{printf $2}')
 
 echo " adding ntp and syncing time"
 pdsh -l root -w $host_list 'yum install -y ntp; ntpdate -s 0.centos.pool.ntp.org; systemctl start ntpd' > /dev/null 2>&1
@@ -60,51 +50,40 @@ pdsh -l root -w $host_list 'yum install -y ntp; ntpdate -s 0.centos.pool.ntp.org
 echo " installing latest docker"
 pdsh -l root -w $host_list 'curl -sSLf https://packages.docker.com/1.12/install.sh | bash;  systemctl enable docker;  systemctl start docker' > /dev/null 2>&1
 
-echo " adding overlay"
+echo " adding overlay storage driver"
 pdsh -l root -w $host_list ' echo "{ \"storage-driver\": \"overlay\"}" > /etc/docker/daemon.json; systemctl restart docker'
 
-echo " starting ucp server."
-ssh root@$manager1 "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --admin-password $password --host-address $manager1" > /dev/null 2>&1
+echo " starting ucp server"
+ssh root@$controller1 "docker run --rm -i --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp install --admin-password $password --host-address $controller1" > /dev/null 2>&1
 
 echo " getting tokens"
-MGRTOKEN=$(ssh root@$manager1 "docker swarm join-token -q manager")
-WRKTOKEN=$(ssh root@$manager1 "docker swarm join-token -q worker")
+MGRTOKEN=$(ssh root@$controller1 "docker swarm join-token -q manager")
+WRKTOKEN=$(ssh root@$controller1 "docker swarm join-token -q worker")
 echo $MGRTOKEN > manager_token.txt
 echo $WRKTOKEN > worker_token.txt
 sleep 10
 
-echo " adding licenses."
-token=$(curl -sk "https://$manager1/auth/login" -X POST -d '{"username":"admin","password":"Pa22word"}'|jq -r .auth_token)
-curl -k "https://$manager1/api/config/license" -X POST -H "Authorization: Bearer $token" -d "{\"auto_refresh\":true,\"license_config\":$(cat $license_file |jq .)}"
+echo " adding license"
+token=$(curl -sk "https://$controller1/auth/login" -X POST -d '{"username":"admin","password":"Pa22word"}'|jq -r .auth_token)
+curl -k "https://$controller1/api/config/license" -X POST -H "Authorization: Bearer $token" -d "{\"auto_refresh\":true,\"license_config\":$(cat $license_file |jq .)}"
 
-echo " setting up mangers"
-pdsh -l root -w $manager2,$manager3 "docker swarm join --token $MGRTOKEN $manager1:2377" > /dev/null 2>&1
+#echo " setting up mangers"
+#pdsh -l root -w $manager2,$manager3 "docker swarm join --token $MGRTOKEN $controller1:2377" > /dev/null 2>&1
 
 sleep 10
-echo " setting up nodes."
-node_list=$(cat hosts.txt |sed -n 3,"$num"p|awk '{printf $1","}')
-pdsh -l root -w $node_list "docker swarm join --token $WRKTOKEN $manager1:2377" > /dev/null 2>&1
+echo " setting up nodes"
+node_list=$(cat hosts.txt |sed -n 1,"$num"p|awk '{printf $1","}')
+pdsh -l root -w $node_list "docker swarm join --token $WRKTOKEN $controller1:2377" > /dev/null 2>&1
 
 echo " downloading certs"
-AUTHTOKEN=$(curl -sk -d '{"username":"admin","password":"'$password'"}' https://$manager1/auth/login | jq -r .auth_token)
-curl -sk -H "Authorization: Bearer $AUTHTOKEN" https://$manager1/api/clientbundle -o bundle.zip
+AUTHTOKEN=$(curl -sk -d '{"username":"admin","password":"'$password'"}' https://$controller1/auth/login | jq -r .auth_token)
+curl -sk -H "Authorization: Bearer $AUTHTOKEN" https://$controller1/api/clientbundle -o bundle.zip
 
 echo " disableing scheduling on controllers"
-token=$(curl -sk "https://$manager1/auth/login" -X POST -d '{"username":"admin","password":"Pa22word"}'|jq -r .auth_token)
-curl -k "https://$manager1/api/config/scheduling" -X POST -H "Authorization: Bearer $token" -d "{\"enable_admin_ucp_scheduling\":false,\"enable_user_ucp_scheduling\":false}"
+token=$(curl -sk "https://$controller1/auth/login" -X POST -d '{"username":"admin","password":"Pa22word"}'|jq -r .auth_token)
+curl -k "https://$controller1/api/config/scheduling" -X POST -H "Authorization: Bearer $token" -d "{\"enable_admin_ucp_scheduling\":true,\"enable_user_ucp_scheduling\":false}"
 
 sleep 20
-echo " installing DTR"
-unzip bundle.zip > /dev/null 2>&1
-curl -sk https://$manager1/ca > ucp-ca.pem
-eval $(<env.sh)
-ssh -t root@$dtr_server "curl -sk https://$manager1/ca > /root/ucp-ca.pem; docker run -it --rm docker/dtr install --ucp-url https://$manager1 --ucp-node $dtr_node --dtr-external-url $dtr_server --ucp-username admin --ucp-password $password --ucp-ca '$(cat ucp-ca.pem)' "
-#--nfs-storage-url nfs://$dtr_server/opt
-curl -sk https://$dtr_server/ca > dtr-ca.pem
-
-#echo " adding second dtr"
-#replica_id=$(docker inspect $(docker ps|grep dtr|grep registry|awk '{print $1}')| jq .[].Config.Env|grep DTR_REPLICA_ID|head -1|sed -e 's/"//g' -e 's/,//g'|awk -F"=" '{print $2}')
-#docker run -it --rm dockerhubenterprise/dtr:2.1.0-beta3 join --existing-replica-id $replica_id --ucp-url https://$manager1 --ucp-node $dtr2_node --ucp-username admin --ucp-password $password --ucp-ca "$(cat ucp-ca.pem)"> /dev/null 2>&1
 
 echo " setting up minio"
 ssh root@$dtr_server 'chmod -R 777 /opt/; docker run -d -p 9000:9000 --name minio minio/minio server /opt' > /dev/null 2>&1
@@ -113,12 +92,21 @@ echo $min_access > min_access.txt
 min_secret=$(ssh root@$dtr_server "docker logs minio |grep SecretKey |awk '{print \$2}'")
 echo $min_secret > min_secret.txt
 
-#echo " building nfs server for dtr"
-#ssh root@$dtr_server 'chmod -R 777 /opt/; systemctl enable rpcbind nfs-server; systemctl start rpcbind nfs-server ; echo "/opt *(rw,sync,no_root_squash,no_all_squash)" > /etc/exports; systemctl restart nfs-server  ' > /dev/null 2>&1
+echo " building nfs server for dtr"
+ssh root@$dtr_server 'chmod -R 777 /opt/; systemctl enable rpcbind nfs-server; systemctl start rpcbind nfs-server ; echo "/opt *(rw,sync,no_root_squash,no_all_squash)" > /etc/exports; systemctl restart nfs-server  ' > /dev/null 2>&1
 
+echo " installing DTR"
+unzip bundle.zip > /dev/null 2>&1
+curl -sk https://$controller1/ca > ucp-ca.pem
+
+eval $(<env.sh)
+#ssh -t root@$dtr_server "curl -sk https://$controller1/ca > /root/ucp-ca.pem; docker run -it --rm docker/dtr install --ucp-url https://$controller1 --ucp-node $dtr_node --dtr-external-url $dtr_server --ucp-username admin --ucp-password $password --ucp-ca '$(cat ucp-ca.pem)' "
+docker run -it --rm docker/dtr install --ucp-url https://$controller1 --ucp-node $dtr_node --dtr-external-url $dtr_server --ucp-username admin --ucp-password $password --ucp-ca "$(cat ucp-ca.pem)"
+#--nfs-storage-url nfs://$dtr_server/opt
+curl -sk https://$dtr_server/ca > dtr-ca.pem
 
 #Add DTR CA to all the nodes (ALL):
-pdsh -l root -w $node_list "curl -sk https://$dtr_server/ca -o /etc/pki/ca-trust/source/anchors/$dtr_server.crt; update-ca-trust; service docker restart" > /dev/null 2>&1
+pdsh -l root -w $node_list "curl -sk https://$dtr_server/ca -o /etc/pki/ca-trust/source/anchors/$dtr_server.crt; update-ca-trust; systemctl restart docker" > /dev/null 2>&1
 
 #notary notes
 #add dtr_ca.pem to all the nodes.
@@ -154,13 +142,13 @@ rm -rf *.txt *.log *.zip *.pem *.pub env.* backup.tar
 
 ############################# status ################################
 function status () {
-  manager1=$(cat hosts.txt|head -1|awk '{print $1}')
-  dtr_server=$(cat hosts.txt|sed -n 4p|awk '{printf $1}')
+  controller1=$(cat hosts.txt|head -1|awk '{print $1}')
+  dtr_server=$(cat hosts.txt|sed -n 2p|awk '{printf $1}')
   echo "===== Cluster ====="
   doctl compute droplet list |grep $prefix
   echo ""
   echo "===== Dashboards ====="
-  echo " - UCP   : https://$manager1"
+  echo " - UCP   : https://$controller1"
   echo " - username : admin"
   echo " - password : "$password
   echo ""
