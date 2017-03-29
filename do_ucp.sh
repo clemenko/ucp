69de563b41d0d0abc145c3755c90bd8a2eb87ee8
Andy Clemenko <clemenko@gmail.com>
2017-03-29 14:37:11 -0400

    added EE bits.

--- do_ucp.sh
+++ do_ucp.sh
@@ -2,6 +2,7 @@
 ###################################
 # edit vars
 ###################################
+set -eu
 num=4  #3 or larger please!
 prefix=ddc
 password=Pa22word
@@ -9,9 +10,9 @@ zone=nyc1
 size=2gb
 key=30:98:4f:c5:47:c2:88:28:fe:3c:23:cd:52:49:51:01
 image=centos-7-x64
-#image=ubuntu-16-04-x64
 password=Pa22word
 license_file="docker_subscription.lic"
+ee_url=$(cat url.env)
 
 ######  NO MOAR EDITS #######
 ################################# up ################################
@@ -31,24 +32,34 @@ doctl compute droplet list|grep -v ID|grep $prefix|awk '{print $3" "$2}'> hosts.
 sleep 10
 
 echo -n " checking for ssh"
-for ext in $(cat hosts.txt|awk '{print $1}'); do
+for ext in $(awk '{print $1}' hosts.txt); do
   until [ $(ssh -o ConnectTimeout=1 root@$ext 'exit' 2>&1 | grep 'timed out' | wc -l) = 0 ]; do echo -n "." ; sleep 5; done
 done
 echo ""
 
-host_list=$(cat hosts.txt|awk '{printf $1","}'|sed 's/,$//')
+host_list=$(awk '{printf $1","}' hosts.txt|sed 's/,$//')
 
 #setting nodes
-controller1=$(cat hosts.txt|sed -n 1p|awk '{print $1}')
-dtr_server=$(cat hosts.txt|sed -n 2p|awk '{printf $1}')
-dtr_node=$(cat hosts.txt|sed -n 2p|awk '{printf $2}')
-hrm_server=$(cat hosts.txt|sed -n 3p|awk '{printf $1}')
+controller1=$(sed -n 1p hosts.txt|awk '{print $1}')
+dtr_server=$(sed -n 2p hosts.txt|awk '{printf $1}')
+dtr_node=$(sed -n 2p hosts.txt|awk '{printf $2}')
+hrm_server=$(sed -n 3p hosts.txt|awk '{printf $1}')
+
+echo " updating dns"
+doctl compute domain records create shirtmullet.com --record-type A --record-name ucp --record-data $controller1 > /dev/null 2>&1
+doctl compute domain records create shirtmullet.com --record-type A --record-name dtr --record-data $dtr_server > /dev/null 2>&1
+doctl compute domain records create shirtmullet.com --record-type A --record-name pets --record-data $hrm_server > /dev/null 2>&1
+doctl compute domain records create shirtmullet.com --record-type A --record-name admin --record-data $hrm_server > /dev/null 2>&1
+doctl compute domain records create shirtmullet.com --record-type A --record-name flask --record-data $hrm_server > /dev/null 2>&1
 
 echo " adding ntp and syncing time"
 pdsh -l root -w $host_list 'yum update -y; yum install -y ntp; ntpdate -s 0.centos.pool.ntp.org; systemctl start ntpd' > /dev/null 2>&1
 
 #echo -n " enabling selinux"
 #pdsh -l root -w $host_list 'sed -i s%disabled%enforcing% /etc/sysconfig/selinux; sed -i s%disabled%enforcing% /etc/selinux/config;shutdown now -r' > /dev/null 2>&1
+
+#echo " updating kernel"
+#pdsh -l root -w $host_list 'rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm; yum --enablerepo=elrepo-kernel install -y kernel-ml; grub2-set-default 1; reboot' > /dev/null 2>&1
 
 #sleep 60
 
@@ -57,8 +68,13 @@ pdsh -l root -w $host_list 'yum update -y; yum install -y ntp; ntpdate -s 0.cent
 #done
 #echo ""
 
-echo " installing latest docker"
-pdsh -l root -w $host_list 'curl -sSLf https://packages.docker.com/1.13/install.sh | bash;  systemctl enable docker;  systemctl start docker' > /dev/null 2>&1
+
+echo " installing docker ee"
+#pdsh -l root -w $host_list 'curl -sSLf https://packages.docker.com/1.13/install.sh | bash;  systemctl enable docker;  systemctl start docker' > /dev/null 2>&1
+
+#EE
+pdsh -l root -w $host_list 'yum install -y yum-utils; echo "'$ee_url'" > /etc/yum/vars/dockerurl; echo "7" > /etc/yum/vars/dockerosversion; yum-config-manager --add-repo $(cat /etc/yum/vars/dockerurl)/docker-ee.repo; yum makecache fast; yum -y install docker-ee; systemctl start docker' > /dev/null 2>&1
+
 
 echo " adding overlay storage driver"
 #pdsh -l root -w $host_list ' echo "{ \"storage-driver\": \"overlay\"}" > /etc/docker/daemon.json; systemctl restart docker'
@@ -85,7 +101,7 @@ curl -k "https://$controller1/api/config/license" -X POST -H "Authorization: Bea
 
 sleep 10
 echo " setting up nodes"
-node_list=$(cat hosts.txt |sed -n 1,"$num"p|awk '{printf $1","}')
+node_list=$(sed -n 1,"$num"p hosts.txt|awk '{printf $1","}')
 pdsh -l root -w $node_list "docker swarm join --token $WRKTOKEN $controller1:2377" > /dev/null 2>&1
 
 echo " downloading certs"
@@ -95,27 +111,20 @@ curl -sk -H "Authorization: Bearer $AUTHTOKEN" https://$controller1/api/clientbu
 sleep 20
 
 echo " building nfs server for dtr"
-ssh root@$dtr_server 'chmod -R 777 /opt/; systemctl enable rpcbind nfs-server; systemctl start rpcbind nfs-server ; echo "/opt *(rw,sync,no_root_squash,no_all_squash)" > /etc/exports; systemctl restart nfs-server  ' > /dev/null 2>&1
+ssh root@$dtr_server 'chmod -R 777 /opt/; yum -y install nfs-utils; systemctl enable rpcbind nfs-server; systemctl start rpcbind nfs-server ; echo "/opt *(rw,sync,no_root_squash,no_all_squash)" > /etc/exports; systemctl restart nfs-server' > /dev/null 2>&1
 
 echo " installing DTR"
 unzip bundle.zip > /dev/null 2>&1
 curl -sk https://$controller1/ca > ucp-ca.pem
 
 eval $(<env.sh)
-docker run -it --rm docker/dtr install --ucp-url https://$controller1 --ucp-node $dtr_node --dtr-external-url https://$dtr_server --ucp-username admin --ucp-password $password --ucp-ca "$(cat ucp-ca.pem)"  > /dev/null 2>&1
+docker run -it --rm docker/dtr install --ucp-url https://$controller1 --ucp-node $dtr_node --dtr-external-url https://dtr.shirtmullet.com --ucp-username admin --ucp-password $password --ucp-ca "$(cat ucp-ca.pem)"  > /dev/null 2>&1
 #--nfs-storage-url nfs://$dtr_server/opt
 curl -sk https://$dtr_server/ca > dtr-ca.pem
 
 echo " disabling scheduling on controllers"
 token=$(curl -sk "https://$controller1/auth/login" -X POST -d '{"username":"admin","password":"'$password'"}'|jq -r .auth_token)
 curl -k --user admin:$password "https://$controller1/api/config/scheduling" -X POST -H "Authorization: Bearer $token" -d "{\"enable_admin_ucp_scheduling\":true,\"enable_user_ucp_scheduling\":false}"
-
-#echo " setting up minio"
-#ssh root@$dtr_server 'chmod -R 777 /opt/; docker run -d -p 9000:9000 --name minio minio/minio server /opt' > /dev/null 2>&1
-#min_access=$(ssh root@$dtr_server "docker logs minio |grep AccessKey |awk '{print \$2}'")
-#echo $min_access > min_access.txt
-#min_secret=$(ssh root@$dtr_server "docker logs minio |grep SecretKey |awk '{print \$2}'")
-#echo $min_secret > min_secret.txt
 
 #echo " enabling scanning engine"
 #curl -k --user admin:$password "https://$dtr_server/api/v0/meta/settings" -X POST -H 'Content-Type: application/json;charset=utf-8' -d "{\"disableBackupWarning\": true,\"scanningEnabled\":true,\"scanningSyncOnline\": true}" > /dev/null 2>&1
@@ -137,15 +146,15 @@ pdsh -l root -w $node_list "curl -sk https://$dtr_server/ca -o /etc/pki/ca-trust
 #    --cacert ${DOCKER_CERT_PATH}/ca.pem \
 #    ${UCP_URL}/info | jq "."
 
-echo " updating dns"
-doctl compute domain records create shirtmullet.com --record-type A --record-name ucp --record-data $controler1 > /dev/null 2>&1
-doctl compute domain records create shirtmullet.com --record-type A --record-name dtr --record-data $dtr_server > /dev/null 2>&1
-doctl compute domain records create shirtmullet.com --record-type A --record-name pets --record-data $hrm_server > /dev/null 2>&1
-doctl compute domain records create shirtmullet.com --record-type A --record-name admin --record-data $hrm_server > /dev/null 2>&1
-doctl compute domain records create shirtmullet.com --record-type A --record-name flask --record-data $hrm_server > /dev/null 2>&1
-
 echo " adding load balancer for worker nodes - this can take a minute or two"
 doctl compute load-balancer create --name lb1 --region $zone --algorithm least_connections --sticky-sessions type:none --forwarding-rules entry_protocol:http,entry_port:80,target_protocol:http,target_port:80 --health-check protocol:tcp,port:80 --droplet-ids $(doctl compute droplet list|grep -v ID|sed -n 2,4p |awk '{printf $1","}'|sed 's/.$//') > /dev/null 2>&1;
+
+echo " setting up minio"
+ssh root@$dtr_server 'chmod -R 777 /opt/; docker run -d -p 9000:9000 --name minio minio/minio server /opt' > /dev/null 2>&1
+min_access=$(ssh root@$dtr_server "docker logs minio |grep AccessKey |awk '{print \$2}'")
+echo $min_access > min_access.txt
+min_secret=$(ssh root@$dtr_server "docker logs minio |grep SecretKey |awk '{print \$2}'")
+echo $min_secret > min_secret.txt
 
 echo ""
 echo "========= UCP install complete ========="
@@ -157,8 +166,8 @@ status
 function kill () {
 echo " killing it all."
 #doctl
-for i in $(cat hosts.txt|awk '{print $2}'); do doctl compute droplet delete --force $i; done
-for i in $(cat hosts.txt|awk '{print $1}'); do ssh-keygen -q -R $i > /dev/null 2>&1; done
+for i in $(awk '{print $2}' hosts.txt); do doctl compute droplet delete --force $i; done
+for i in $(awk '{print $1}' hosts.txt); do ssh-keygen -q -R $i > /dev/null 2>&1; done
 for i in $(doctl compute domain records list shirtmullet.com|grep 'pets\|admin\|flask\|ucp\|dtr'|awk '{print $1}'); do doctl compute domain records delete shirtmullet.com $i; done
 doctl compute load-balancer delete -f $(doctl compute load-balancer list|grep -v ID|awk '{print $1}') > /dev/null 2>&1;
 
@@ -167,8 +176,8 @@ rm -rf *.txt *.log *.zip *.pem *.pub env.* backup.tar
 
 ############################# status ################################
 function status () {
-  controller1=$(cat hosts.txt|head -1|awk '{print $1}')
-  dtr_server=$(cat hosts.txt|sed -n 2p|awk '{printf $1}')
+  controller1=$(head -1 hosts.txt|awk '{print $1}')
+  dtr_server=$(sed -n 2p hosts.txt|awk '{printf $1}')
   echo "===== Cluster ====="
   doctl compute droplet list |grep $prefix
   echo ""
